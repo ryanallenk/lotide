13843c1898cb9fb39b5d233c6d94bffa87080134
ryanallenk <ryanallenk@gmail.com>
2022-03-07 18:19:46 -0500

    Created an console assert equivalent

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertation Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertation Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("My test", "My test");
+assertEqual(12, 1);
