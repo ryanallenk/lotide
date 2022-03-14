41f4b0516f645ea9fa0620c8040478dc0ffdac14
ryanallenk <ryanallenk@gmail.com>
2022-03-14 14:17:43 -0400

    refactored assertEqual and head functions to separate tests

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("My test", "My test");
-assertEqual(12, 1);
+module.exports = assertEqual;
